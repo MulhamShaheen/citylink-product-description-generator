fae8d2099c58067f31f27ca6f81c270673d13013
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2024-06-13 17:25:01 +0300

    feat: add params to prompt

--- apps/backend/core/model/prompts/marketing/__init__.py
+++ apps/backend/core/model/prompts/marketing/__init__.py
@@ -1,3 +1,4 @@
 from core.model.prompts.marketing.template import MarketingPromptTemplate
 
 MarketingPromptTemplate.set_template()
+MarketingPromptTemplate.set_parameters({"product_info": str})
--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,4 +1,5 @@
 from core.model.client import YGPTClient
+from core.model.prompts.marketing import MarketingPromptTemplate
 
-
-YGPTClient.generate("What is the meaning of life?", 1000)
+print(MarketingPromptTemplate.generate_prompt(product_info="product_info"))
+# YGPTClient.generate("What is the meaning of life?", 1000)
